c06d076a10926c46c6a508f39435c83f804f77aa
Пользователь <grigoryan_jenya@bk.ru>
2022-02-01 21:20:00 +0400

    message

--- header.js
+++ header.js
@@ -1,23 +1,21 @@
-$('.right_menu').click(function(){
-    $('.second_submenu').toggle()
-})
+$(".right_menu").click(function () {
+  $(".second_submenu").toggle();
+});
 
-let submenu=document.querySelector('.submenu');
-let menu_item=document.querySelector('.category_menu')
-let menu_btn=document.querySelector('.category_menu_btn')
+let submenu = document.querySelector(".submenu");
+let menu_item = document.querySelector(".category_menu");
+let menu_btn = document.querySelector(".category_menu_btn");
 function closeMenu(e) {
-    e.preventDefault()
-    if(submenu.style.display==='none'){
-        submenu.style.display='flex';
-        
-    }else{
-        submenu.style.display='none';
-        
-    }
+  e.preventDefault();
+  if (submenu.style.display === "none") {
+    submenu.style.display = "flex";
+  } else {
+    submenu.style.display = "none";
+  }
 }
 
-menu_item.addEventListener('click',closeMenu)
-menu_btn.addEventListener('click',closeMenu)
-$('.burger_btn').on('click',()=>{
-    $('.burger_menu').toggle()
-})
+menu_item.addEventListener("click", closeMenu);
+menu_btn.addEventListener("click", closeMenu);
+$(".burger_btn").on("click", () => {
+  $(".burger_menu").toggle();
+});
--- script.js
+++ script.js
@@ -1,41 +1,61 @@
-$('.best_offer').slick({
+$(".best_offer").slick({
   slidesToShow: 3,
   infinite: true,
   slidesToScroll: 1,
-    prevArrow: '<div class="slick-prev"><i class="far fa-arrow-circle-left"></i></div>',
-    nextArrow: '<div class="slick-next"><i class="far fa-arrow-circle-right"></i></div>',
-    autoplay: true,
-    autoplaySpeed:2000,
-    mobileFirst: true,
-    responsive: [
-      {
-        breakpoint:1920,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll:1,
-          
-        }
+  prevArrow:
+    '<div class="slick-prev"><i class="far fa-arrow-circle-left"></i></div>',
+  nextArrow:
+    '<div class="slick-next"><i class="far fa-arrow-circle-right"></i></div>',
+  autoplay: true,
+  autoplaySpeed: 2000,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 1920,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
       },
-      {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll:1,
-        }
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
       },
-      {
-        breakpoint: 300,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll:1,
-        }
+    },
+    {
+      breakpoint: 300,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll:1,
-        }
-      }
-    ]
-  });
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+var checkbox = document.querySelector('.tog_switch');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+})
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
