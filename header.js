c0e812e78a96465da84766a436fc73418b0d8b81
Пользователь <grigoryan_jenya@bk.ru>
2022-02-06 22:24:51 +0400

    nor

--- header.js
+++ header.js
@@ -1,6 +1,10 @@
-$(".right_menu").click(function () {
-  $(".second_submenu").toggle();
-});
+
+$(document).ready(()=>{
+  $(".right_menu").click(function () {
+    $(".second_submenu").toggle();
+  });
+  
+})
 
 let submenu = document.querySelector(".submenu");
 let menu_item = document.querySelector(".category_menu");
@@ -19,3 +23,15 @@ menu_btn.addEventListener("click", closeMenu);
 $(".burger_btn").on("click", () => {
   $(".burger_menu").toggle();
 });
+
+let switch_btn=document.getElementById('switch');
+let body=document.body;
+switch_btn.addEventListener('click',()=>{
+  if(body.classList.contains('light')){
+    body.style.backgroundImage='url("img/black_big_bg.jpg")'
+    body.classList.remove('light')
+  }else{
+    body.style.backgroundImage='url("img/Background.jpg")';
+    body.classList.add('light')
+  }
+})
